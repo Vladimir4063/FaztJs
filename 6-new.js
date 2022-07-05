9e2878b5da295b216374b305625d6d977c881c78
Vladimir4063 <vladimirgutierrez4063@gmail.com>
2022-07-05 00:07:29 -0300

    Review property new

---
+++ 6-new.js
@@ -0,0 +1,13 @@
+function Person(){
+    "use strict"
+    this.name = ""
+    this.latname = ""
+}
+// Si no referenciamos con la palabra new, se estará asignando la clase a una clase super interna
+// use strict funciona para evitar este error
+
+const person = new Person()
+person.name = "Vladimir"
+person.latname = "Gutierrez"
+console.log(person.name)
+console.log(person.latname)
