2a46846bcf67df7c3a80affcc7c9d3c21f61ab17
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2024-01-18 10:43:39 -0300

    6 new update

--- 6-new.js
+++ 6-new.js
@@ -1,14 +1,16 @@
-function Person(){
-    "use strict"
-    this.name = ""
-    this.latname = ""
+const mankind = {
+    Person: function Person(){
+        "use strict"
+        this.name = ""
+        this.latname = ""
+    }
 }
-// Si no referenciamos con la palabra new, se estará asignando la clase a una clase super interna
+// Si no referenciamos con la palabra new, se estará asignando la clase a una clase super interna y más elevada
 // use strict funciona para evitar este error
 
-const person = new Person()
-person.name = "Vladimir"
-person.latname = "Gutierrez"
+const person = new mankind.Person()
+// person.name = "Vladimir"
+// person.latname = "Gutierrez"
 console.log(person);
-console.log(person.name)
-console.log(person.latname)
+console.log(mankind)
+// console.log(person.name)
